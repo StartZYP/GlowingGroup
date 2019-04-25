eaa86d180237fd714316980620d3176e401fce44
StartZYP <Code_Hacker@aliyun.com>
2019-04-25 23:14:52 +0800

    更新一下

--- src/com/qq44920040/Minecraft/GlowingGroup/Main.java
+++ src/com/qq44920040/Minecraft/GlowingGroup/Main.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -57,14 +58,18 @@ public class Main extends JavaPlugin implements Listener {
     }
 
 
-//    @EventHandler
-//    public void PlayerDoCommand(Exe){
-//
-//    }
+    @EventHandler
+    public void PlayerDoCommand(PlayerCommandPreprocessEvent event){
+        if (event.getMessage().contains("nuke")){
+            Player player =event.getPlayer();
+            if (PlayerGroup.get(player.getUniqueId())){
+                event.setCancelled(true);
+            }
+        }
+    }
 
     private void Reload(){
         reloadConfig();
-
     }
 
     @Override
